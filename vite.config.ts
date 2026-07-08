0504551072767840e202ab261e01283de6929754
Guilherme Santos <l1ghtmarebr@gmail.com>
2026-07-08 00:02:06 -0300

    Making the deploy in ghpages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/SensShift/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
